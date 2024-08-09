80cfe0b31d8edee365245f0871735f42a8c64e8b
Yash Sanjyot Ainapure <yash@genlord.kali>
2024-08-09 14:38:20 +0530

    2nd practical programs

---
+++ practical2.py
@@ -0,0 +1,83 @@
+import numpy
+
+#print numbers upto n
+# num=int(input("enter the number to print number upto it"))
+# for i in range(1,num+1):
+#    print(i)
+
+
+
+
+# print even numbers upto n number
+# num1=int(input("enter a number to print even numbers upto it"))
+# for i in range(1,num1+1):
+#    if i%2==0:
+#       print(i)
+
+
+
+
+# print odd numbers upto n number
+# num2=int(input("enter a number to print odd numbers upto it"))
+# for i in range(1,num2+1):
+#    if i%2!=0:
+#       print(i)
+
+
+
+# print sum of all numbers upto n
+# num3=int(input("enter a number to print all sum of all upto that number"))
+# sum=0
+# for i in range(1,num3+1):
+#    sum+=i
+# print("sum is: ",sum)   
+
+
+
+
+# print sum of all odd numbers upto n
+# num4=int(input("enter a number to print all sum of all odd numbers upto that number"))
+# sum=0
+# for i in range(1,num4+1):
+#    if i%2!=0:
+#       sum+=i
+# print("sum of all odd numbers is: ",sum)   
+
+
+# multiply table
+# num7=int(input("enter a number to print the table"))
+# for i in range(1,11):
+#    print(i," * ",num7," = ",num7*i)
+
+
+
+
+# string reverse
+# strx="aryan"
+# print("reverse string: ",strx[::-1])
+
+
+
+
+
+# prime number
+# num5=int(input("enter a number to all prime numbers in that range"))
+# isPrime=True
+# for i in range(2,num5+1):
+#    if num5<=1:
+#       break
+#    for j in range(2,i):
+#       if i%j==0:
+#          isPrime=False
+#    if isPrime:
+#       print(i," is a prime no.")      
+#    else:
+#       isPrime=True   
+
+
+
+
+
+# reverse the number
+# num6=int(input("enter the number to reverse it: "))
+# digit=int(numpy.log10(num6))
